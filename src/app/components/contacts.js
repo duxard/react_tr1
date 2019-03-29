6d25b1f03decd6da56a70e57560e1a790fe19e9c
duxard <duxard@gmail.com>
2019-03-29 23:10:19 +0300

    phase 2

--- src/app/components/contacts.js
+++ src/app/components/contacts.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import '../css/contacts.css';
+
+const fruit = ["pear", "apple", "plum", "kiwi"];
 
 export default class Contacts extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            fruitList: []
+        };
+    }
+
     componentDidMount(){
-        console.log( this.props.names );
+        this.setState({
+            fruitList: fruit
+        }, function(){
+            console.log(this.state.fruitList);
+        });
     }
 
     render(){
         return (
             <div>
                 <h2>Contacts page</h2>
+                <p>Owners: {this.props.names[0]} : {this.props.names[1]}</p>
+                <ul id="list">
+                    {
+                        this.state.fruitList.map((item, index) => <li key={index}>{item}</li>)
+                    }
+                </ul>
+                <hr />
+                <h3>Component with children:</h3>
+                <hr />
             </div>
         );
     }
